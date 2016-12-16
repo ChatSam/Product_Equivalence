245ca566b9f4d1c3804bbed56153ba83667ed623
ChatSam <chaturacs@gmail.com>
2016-12-16 10:14:37 -0500

    version 1.0 completed

--- product_equivalence.py
+++ product_equivalence.py
@@ -8,50 +8,61 @@ Invocation:
 	Prod = product_equivalence()
 	are_products_identical(market_place1, product_id1, market_place2, product_id2)
 """
-
 import requests
 import json
 from collections import Counter 
 
+class ProdEqualError(Exception):
+	"""base class for custom exception for ProdEqual related errors"""
+	def __init__(self, message):
+		self.message = __class__.__name__+': '+message
+	
+class ZincAPIError(ProdEqualError):
+	"""custom exception for Zinc API related errors"""
+	pass
+
+class ProdEqualInternalError(ProdEqualError):
+	"""custom exception for ProdEqual internal errors"""
+	pass
+
+class ProdEqualHttpRequestError(ProdEqualError):
+	"""custom exception for http request errors"""
+	pass
 
 class product_equivalence:
-	""" main method which checks the equivalence of two products
-		return - boolean
-	"""
 	
-	_auth_key = 'A2984FCCADEC3B7AE3159DCB'
-	_api_string = 'https://api.zinc.io/v1/products/{prod_id}?retailer={market_place}'
+	_AUTH_KEY = 'A2984FCCADEC3B7AE3159DCB'
+	_API_STRING = 'https://api.zinc.io/v1/products/{prod_id}?retailer={market_place}'
 	_ok_status = 200;
 
-	def are_products_identical(self, market_p1, product_id1, market_p2, product_id2):
 
+	def are_products_identical(self, market_p1, product_id1, market_p2, product_id2):
+		""" main method which checks the equivalence of two products
+			returns: boolean value
+		"""
 		#retrieve data from the Zinc Api
-		prod1_obj = self.retriveData(market_p1,product_id1)
-		
-		prod2_obj = self.retriveData(market_p2,product_id2)
+		prod1_obj = self.retrive_data(market_p1,product_id1)
+		prod2_obj = self.retrive_data(market_p2,product_id2)
 
 		prod1_index = {}
-
 		prod2_index = {}
 
 		if(prod1_obj and prod2_obj):
+
 			#create indexes to compare values of each product
 			prod1_index = self.create_product_index(prod1_obj)
-
 			prod2_index = self.create_product_index(prod2_obj)
 
 			standard_epids = ['ISBN','UPC','MPN']
 
-		#if both products have search indexes
-		if(prod1_index and prod2_index):
-			
-			for epid in standard_epids:
-				#print(prod1_epids.keys())
-				#print(prod2_epids)
+		elif(prod1_obj == None or prod2_obj == None):
+			return None
 
+		#if both products have search indexes
+		if(prod1_index and prod2_index):	
+			for epid in standard_epids:
 				if(epid in prod1_index.keys() and epid in prod2_index.keys()):
 					if(prod1_index[epid] == prod2_index[epid]):
-						#print ("prod1 {0} | prod2 {1}".format(prod1_epids[epid],prod2_epids[epid] ))
 						return True
 
 		return False
@@ -59,71 +70,64 @@ class product_equivalence:
 
 	def create_product_index(self,prod_obj):
 		"""creates a searchable product details index -return dictionary of product details
-			return - product details index dictionary
+			returns: product details indexed in a dictionary
 		"""
-
 		MPN_alias = 'Item model number'
-
+		
 		# key for the MPN_alias value in the search index
-		MPN_second = 'MPN_second'
+		MPN_second = 'MPN'
 
 		#search index
 		prod_index = {}
 
+		# add the epids to the search index
+		if('epids' in prod_obj):
+			for item in prod_obj['epids']:
+				prod_index[item['type']] = item['value'].strip()
+
 		# add the product details to the search index
 		if ('product_details' in prod_obj):
-
 			prod1_details_list = prod_obj['product_details'];
 
 			#extracting the items in product details which match the value in MPN_alias
 			prod_MPN_value = [prod_item for prod_item in prod1_details_list if MPN_alias in prod_item]
-
-			#if exists, format the first MPN_alias value and add it to the search index
-			if(prod_MPN_value):
- 
+			
+			#if exists, format the first MPN_alias value and replace it for MPN
+			if(prod_MPN_value and MPN_second not in prod_index.keys()):
 				prod_index[MPN_second] = prod_MPN_value[0].split(":")[1].strip()
-
-		# add the epids to the search index
-		if('epids' in prod_obj):
-
-			for item in prod_obj['epids']:
-
-				prod_index[item['type']] = item['value'].strip()
 
 		return prod_index
 
  
-	def retriveData(self, market_p, product_id):
+	def retrive_data(self, market_p, product_id):
 		""" retrives the product data from the Zinc API
-			return - dictionary with all the details of the product
+			returns: dictionary with all the details of the product
 		"""
-
 		#creating the request url for product
-		prod_request = self._api_string.replace('{prod_id}',product_id)
+		prod_request = self._API_STRING.replace('{prod_id}',product_id)
 		prod_request = prod_request.replace('{market_place}',market_p)
+		
+		try:
+			#api response for product name
+			prod_response = requests.get(prod_request,auth = (self._AUTH_KEY,''))
+		except KeyboardInterrupt:
+			exit()
+		except:
+			raise ProdEqualHttpRequestError('bad request')
 
-		#response for prod1
-		prod_response = requests.get(prod_request,auth = (self._auth_key,''))
-
+		#data object to be returned
 		prod_obj = None
 
 		if(prod_response.status_code == self._ok_status):
+			
 			#parse response to python object
 			try:
 				prod_obj = prod_response.json()
+
+				if(prod_obj['status'] == 'failed'):
+					raise ZincAPIError('Unable to fetch product data from the Zinc API.'+
+										' Make sure the product_id and the market_place values are correct.')
 			except ValueError:
-				print ('cannot load the product details due to json error')
-		else:
-			#todo: write a proper request
-			print("Error in the request")
-			exit()
-
+				raise ProdEqualInternalError('Cannot load the product details due to an internal error (json issue)')
+		
 		return prod_obj
-
-
-Prod = product_equivalence()
-#print(Prod.are_products_identical("bhphotovideo","1223091-REG","amazon","B007W5B7IK"))
-print(Prod.are_products_identical("bhphotovideo","1148315-REG","amazon","B00PB1K3KA"));
-#Prod.are_products_identical("bhphotovideo","1146603-REG","amazon","B007B5TMW4")
-
-
--- unitTests.py
+++ unitTests.py
@@ -3,53 +3,76 @@ Test file for product_equivalence module
 Author: Chatura Samarasinghe
 
 """
+import product_equivalence as pro
+import unittest
 
-import product_equivalence
+class TestProductEquivalence(unittest.TestCase):
 
-filename = "bhphotovideo_to_asin_examples.txt "
-#filename ="test3.txt"
+	prod_eql = pro.product_equivalence()
 
-def initializeForTest():
+	#runs the test cases from the primary test file
+	def test_run_primary(self):		
+		passed = 0
+		failed = 0
+		#filename = "bhphotovideo_to_asin_examples.txt"
+		filename = "test2.txt"
+		testCases = self.initialize_for_test(filename)
 
-	file = open(filename)
+		print("running main test - {0} testcases ...".format(len(testCases)))
 
-	testList = list(file)
+		for testUnit in testCases:
 
-	count = 0;
+			try:
+				prod_test_run = self.prod_eql.are_products_identical(testUnit[0],testUnit[1],testUnit[2],testUnit[3])
+				test_no = testCases.index(testUnit) + 1
 
-	passed = 0
+				self.assertTrue(prod_test_run)
 
-	failed = 0
-
-	for item in testList:
-		
-		test = item.strip()
-
-		if(test):
-			count += 1
-
-			testUnit =test.split()
-
-
-			prodEq = product_equivalence.product_equivalence()
-
-			#invoke function
-			prod_test_run = prodEq.are_products_identical(testUnit[0],testUnit[1],testUnit[2],testUnit[3])
-
-			if(prod_test_run):
-				print("test case: {0} | output {1}".format(count,prod_test_run))
-				passed += 1		
+			except AssertionError as err:
+				print ("test case: {0} | output {1} -> bhphotovideo {2} amazon {3}"
+					.format(test_no, prod_test_run, testUnit[1], testUnit[3]))
+				failed += 1
+			
+			except pro.ProdEqualError as err:
+				print(err.message)
 
 			else:
-				print ("test case: {0} | output {1} -> bhphotovideo {2} amazon {3}".format(count, prod_test_run, testUnit[1], testUnit[3]))
-				failed += 1
+				print("test case: {0} | output {1}".format(test_no,prod_test_run))
+				passed += 1
 
-	accuracy = float((passed/count) * 100)
+		print ("passed: {0} | failed: {1}".format(passed, failed))
 
-	print ("passed: {0} | failed: {1} | est accuracy: {2}%".format(passed, failed, accuracy))
+	#tests for custom exception
+	def test_run_excep(self):
 
-	file.close()
+		testUnit = ["bhphotovideo","AAAAA114835-REG","amazon","B00PB1K3KA"]
 
+		try:
+			prod_test_run = self.prod_eql.are_products_identical(testUnit[0],testUnit[1],testUnit[2],testUnit[3])
+		except pro.ProdEqualError:
+			self.assertTrue(True)
+			pass
+		else:
+			self.assertTrue(False)
+			pass
 
+	#loads the testcase from file
+	def initialize_for_test(self,filename):
+		testCases = []
+		file = open(filename)
+		testList = list(file)
 
-initializeForTest()
+		for item in testList:		
+			test = item.strip()
+
+			if(test):
+				testUnit =test.split()
+
+				testCases.append(testUnit)		
+
+		file.close()
+
+		return testCases
+
+if __name__ == '__main__':
+    unittest.main()
